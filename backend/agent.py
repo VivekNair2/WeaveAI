9142ce209e52a2863cb051bdbc6e83278806a027
VedantLaptop <vedant.naik09@gmail.com>
2025-03-22 18:49:05 +0530

    Hindi removed

--- backend/agent.py
+++ backend/agent.py
@@ -114,7 +114,7 @@ class WebAgent:
             instructions=[
                 "Given a topic by the user, respond with 4 latest search items about that topic.",
                 "Search for 10  items and select the top 4 unique items.",
-                "Search in English and Hindi"
+                "Search in English."
             ],
             show_tool_calls=True,
             debug_mode=True,
